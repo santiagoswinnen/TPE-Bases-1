f965f6f804bf23dfb3650f88c7488cd0a7667e28
Santiago Swinnen <sswinnen@itba.edu.ar>
2018-06-16 03:09:13 -0300

    Trigger fixed

--- trigger.sql
+++ trigger.sql
@@ -1,12 +1,27 @@
-CREATE TRIGGER insercion BEFORE INSERT
-ON recorrido_final FOR STATEMENT
-EXECUTE PROCEDURE trigger_func();
--- La idea de este trigger es chequear para futuras inserciones que se cumplan las restricciones
+
+-- La funcion de este trigger es chequear para futuras inserciones que se cumplan las restricciones
 CREATE OR REPLACE FUNCTION trigger_func()
 RETURNS TRIGGER
 AS $$
+DECLARE
+  sobreps INTEGER;
   BEGIN
-    --Codigo del trigger
-  END;
-$$
+        sobreps = (SELECT COUNT(*)
+        FROM recorrido_final
+        WHERE NEW.usuario = usuario AND (((NEW.fecha_hora_dev = fecha_hora_ret) OR (fecha_hora_dev = NEW.fecha_hora_ret))
+                                         OR ((NEW.fecha_hora_dev > fecha_hora_ret AND NEW.fecha_hora_ret < fecha_hora_ret)
+                                             OR (fecha_hora_dev > NEW.fecha_hora_ret AND fecha_hora_ret < NEW.fecha_hora_ret))
+                                         OR (NEW.fecha_hora_ret = fecha_hora_ret)
+                                         OR ((NEW.fecha_hora_ret > fecha_hora_ret AND NEW.fecha_hora_dev < fecha_hora_dev)
+                                             OR (fecha_hora_ret>NEW.fecha_hora_ret AND fecha_hora_dev < NEW.fecha_hora_dev))
+                                         OR (fecha_hora_dev = NEW.fecha_hora_dev)
+                                         OR ((NEW.fecha_hora_ret = fecha_hora_ret) AND (NEW.fecha_hora_dev = fecha_hora_dev))));
 
+        IF sobreps > 0 THEN RAISE EXCEPTION 'Se ha producido un error en la inserción: ' ||
+                                            'El intervalo se solapa'; END IF;
+				RETURN NEW;
+
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insercion BEFORE INSERT ON recorrido_final FOR EACH ROW EXECUTE PROCEDURE trigger_func();
